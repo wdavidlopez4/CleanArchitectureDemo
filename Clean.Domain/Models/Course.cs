39c19b08fd215a456e2a8e5523c42bdfc1aa3da9
David Lopez <wdavidlopez4@gmail.com>
2020-06-22 15:26:15 -0500

    dentro de la capa de domio se creo el componente models, el contendra todos los modelos de dominio

---
+++ Clean.Domain/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Clean.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DescriptionAttribute { get; set; }
+
+        public string ImagenURL { get; set; }
+    }
+}
