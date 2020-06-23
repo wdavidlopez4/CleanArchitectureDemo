0e59e999570b908d934ef34bc36c9513d76e732a
David Lopez <wdavidlopez4@gmail.com>
2020-06-23 16:25:02 -0500

    se creo la vista de los cursos y se agrego el css para darle estilo

---
+++ Clean.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Clean.ApplicationCore.Interfaces;
+using Clean.ApplicationCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseVM courseVM = courseService.GetCourse();
+            return View(courseVM);
+        }
+    }
+}
