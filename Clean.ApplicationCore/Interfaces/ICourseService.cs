331b1cde0895f169cf511f1bf68f04a1185016f0
David Lopez <wdavidlopez4@gmail.com>
2020-06-22 19:43:37 -0500

    en la capa de aplicacion en el componente vm se agrego el coursevm y en la interfas se creo el contrato ICourseService

---
+++ Clean.ApplicationCore/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using Clean.ApplicationCore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.ApplicationCore.Interfaces
+{
+    public interface ICourseService
+    {
+        public IEnumerable<CourseVM> GetCourse();
+    }
+}
---
+++ Clean.ApplicationCore/ViewModels/CourseVM.cs
@@ -0,0 +1,12 @@
+﻿using Clean.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.ApplicationCore.ViewModels
+{
+    public class CourseVM
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
