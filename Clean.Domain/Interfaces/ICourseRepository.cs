ca5897b8f97fe6ba971829e91e703233b4c9b774
David Lopez <wdavidlopez4@gmail.com>
2020-06-22 19:51:28 -0500

    se crearon las interfaces de la capa de dominio

---
+++ Clean.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using Clean.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clean.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        public IEnumerable<Course> GetCourses();
+    }
+}
